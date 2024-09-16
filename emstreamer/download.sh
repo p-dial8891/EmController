a7276775c3eeed3184ba0955004829e6e4573d9b
Philip <122394571+p-dial8891@users.noreply.github.com>
2024-09-16 13:38:54 +0100

    Delete emstreamer directory

--- emstreamer/download.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/bash
-rsync --ignore-existing -r -u -v --rsh=ssh -e 'ssh -i ~/EmController/emstreamer/EC2-Trial1-Keys.pem' --exclude=*.m4v  --exclude='Music/iTunes/Album Artwork/**'  "ubuntu@ec2-13-53-136-123.eu-north-1.compute.amazonaws.com:/home/ubuntu/Audio" ~/EmController/music
