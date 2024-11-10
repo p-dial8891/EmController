ce334b29c03e4ed69e7ee51d826277a550edc55d
Philip Thomas <proteus>
2024-11-11 04:20:12 +0530

    Added uinput device creation.

--- controller-rs/src/main.rs
+++ controller-rs/src/main.rs
@@ -1,4 +1,6 @@
 use evdev::InputEventKind::Key;
+use evdev::AttributeSetRef;
+use evdev::{uinput::VirtualDeviceBuilder, AttributeSet};
 
 fn main() {
     println!("Hello, world!");
@@ -7,6 +9,14 @@ fn main() {
     let d = evdev::Device::open( "/dev/input/by-id/usb-ZEPHYR_Zephyr_HID_mouse_sample_2735883958566D65-event-mouse" );
     let mut s = d.unwrap();
     println!("{s}");
+    let mut toPlayer = VirtualDeviceBuilder::new().unwrap()
+        .name("fake-mouse")
+        .with_keys(&AttributeSet::from_iter([
+            evdev::Key::BTN_LEFT,
+            evdev::Key::BTN_RIGHT])).unwrap()
+        .build()
+        .unwrap();
+
     loop {
         for e in s.fetch_events().unwrap() {
             if let Key(k) = e.kind() {
